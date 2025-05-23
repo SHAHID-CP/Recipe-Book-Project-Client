4da719622a44b28551ff1a1d7bbe7c9935804888
Black Fighter <blackfighter@gmail.com>
2025-05-23 16:51:47 +0600

    card data pass

--- src/Components/Allrecipe.jsx
+++ src/Components/Allrecipe.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import HomeCard from './Home/HomeCard';
+import { useLoaderData } from 'react-router';
 
 const Allrecipe = () => {
+
+    const recipe= useLoaderData();
     return (
         <div className='my-14'>
             
             <h2 className='text-3xl font-bold mb-12'>All recipes</h2>
             <div className=' grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8'>
-                <HomeCard></HomeCard>
-                <HomeCard></HomeCard>
-                <HomeCard></HomeCard>
-                <HomeCard></HomeCard>
+                
+                {
+                    recipe.map(singleRecipe =>(<HomeCard key={singleRecipe._id} singleRecipe={singleRecipe}></HomeCard>))
+                }
             </div>
         </div>
     );
