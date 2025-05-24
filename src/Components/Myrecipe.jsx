d56d451f2ffa2bd3b1bf7a578c1415d9cdea8032
Black Fighter <blackfighter@gmail.com>
2025-05-24 11:41:38 +0600

    recipe delete

--- src/Components/Myrecipe.jsx
+++ src/Components/Myrecipe.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { use, useEffect, useState } from 'react';
 import MyrecipeCard from './MyrecipeCard';
+import { AuthContext } from '../Context/AuthContext';
 
 const Myrecipe = () => {
+    const [recipe,setrecipe] =useState([])
+
+    const {user}= use(AuthContext);
+    const id=user?.email;
+
+    useEffect(()=>{
+        if(id){
+            fetch(`http://localhost:3000/user?email=${id}`)
+            .then(res => res.json())
+            .then(data=> setrecipe(data))
+        }
+    },[id])
+
+    const removeSta= (id)=>{
+        const newData= recipe.filter(item => item._id !== id)
+        setrecipe(newData)
+    }
+
     return (
         <div className='my-14'>
                     <h2 className='text-3xl font-bold mb-8'>My all recipe</h2>
-        
-                    <MyrecipeCard></MyrecipeCard>
-                    <MyrecipeCard></MyrecipeCard>
-        
+
+                    {
+                    recipe.length === 0 && ( <p className='text-5xl text-center font-bold mt-12'>No recipe please add recipe</p>)
+                    }
+                    
+                    {
+                        recipe&& recipe.map(singleRecipe=>(<MyrecipeCard key={singleRecipe._id} singleRecipe={singleRecipe} removeSta={removeSta}></MyrecipeCard>))
+                    }
+                    
                 </div>
     );
 };
--- src/Components/MyrecipeCard.jsx
+++ src/Components/MyrecipeCard.jsx
@@ -5,35 +5,52 @@ import { IoMdTime } from "react-icons/io";
 import { GrUpdate } from "react-icons/gr";
 import { MdDeleteForever } from "react-icons/md";
 
-const MyrecipeCard = () => {
+const MyrecipeCard = ({singleRecipe,removeSta}) => {
+    const {_id,image,title,ingredients,instructions,quisine,preparetiontime,likecount,category}=singleRecipe || {};
 
-    const like=12;
+    const hundleDelete= ()=>{
+            removeSta(_id)
+            fetch(`http://localhost:3000/recipe/${_id}`,{
+                method: "DELETE"
+            })
+            .then(res=> res.json())
+            .then(data=>{
+                if(data.deletedCount){
+                    removeSta(_id)
+                }
+            })
+    }
     return (
         <div>
             
             <div className='border-2 rounded-2xl border-gray-200 p-5 bg-gray-100 sm:flex items-end md:w-10/12 mx-auto mb-8'>
         
                 <div className='sm:w-1/2'>
-                    <img className='w-full max-h-48 sm:max-h-80 rounded-2xl ' src="https://i.ibb.co/xtt85X3T/images.jpg" alt="None" />
+                    <img className='w-full max-h-48 sm:max-h-80 rounded-2xl ' src={image} alt="None" />
                 </div>
                 <div className='mt-2 sm:w-1/2 sm:ml-4'>
-                    <h3 className='text-3xl font-bold text-center mb-8 mt-2'>Italian pasta</h3>
+                    <h3 className='text-3xl font-bold text-center mb-8 mt-2'>{title}</h3>
         
         
         
                     <div className='mb-4 text-lg font-semibold border-b-1 border-gray-300 border-dashed'>
                         Ingredients:
-                        <p className='text-sm pl-2 mb-2'>1 cup sugar</p>
+                        <p className='text-sm pl-2 mb-2'>{ingredients}</p>
                     </div>
         
                     <div className='mb-4 text-lg font-semibold border-b-1 border-gray-300 border-dashed'>
                         Instructions:
-                        <p className='text-sm pl-2 mb-2'>1 cup sugar</p>
+                        <p className='text-sm pl-2 mb-2'>{instructions}</p>
                     </div>
 
 
                     <div className='flex justify-between items-center'>
-                    <p className='bg-[#3b83f698] w-fit px-1 rounded-2xl'> Breackfast</p>
+                    
+                    <div>
+                        {
+                                category.map((singlecat,index)=>(<p key={index} className='bg-[#3b83f698] w-fit px-1 rounded-2xl mb-1'> {singlecat}</p>))
+                        }
+                    </div>
 
                     <button className='cursor-pointer'>
                         <GrUpdate color='green'/>
@@ -41,16 +58,16 @@ const MyrecipeCard = () => {
 
                     </div>
                     <div className='my-4 flex justify-between items-center'>
-                    <p className='flex items-center gap-2 '><IoMdTime color='red' />30 min.</p>
-                    <button className='cursor-pointer'>
+                    <p className='flex items-center gap-2 '><IoMdTime color='red' />{preparetiontime} min.</p>
+                    <button onClick={hundleDelete} className='cursor-pointer'>
                         <MdDeleteForever  color='red' size={24}/>
                     </button>
                     </div>
 
 
                     <div className='flex justify-between items-center'>
-                        <p className='flex items-center gap-2 text-[#34d399]'><GiCampCookingPot />Italian</p>
-                        <p className='flex items-center gap-2  hover:text-[#3b82f6]'><AiFillLike />{like}</p>
+                        <p className='flex items-center gap-2 text-[#34d399]'><GiCampCookingPot />{quisine}</p>
+                        <p className='flex items-center gap-2  hover:text-[#3b82f6]'><AiFillLike />{likecount}</p>
                         
                     </div>
                                     
